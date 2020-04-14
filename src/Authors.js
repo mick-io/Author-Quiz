6a937eda71ec13c7538108ad16801904c027b2da
Mick Piereder <mick.io@pm.me>
2020-04-14 14:01:00 -0600

    first commit

---
+++ src/Authors.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    name: "Mark Twain",
+    imageURL: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Mark_Twain_by_AF_Bradley.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Adventures of HuckleBerry Fin"],
+  },
+  {
+    name: "Joseph Conrad",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e5/Joseph_Conrad%2C_Fotografie_von_George_Charles_Beresford%2C_1904.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Heart of Darkness"],
+  },
+  {
+    name: "J.K. Rowling",
+    imageURL: "https://upload.wikimedia.org/wikipedia/commons/5/5d/J._K._Rowling_2010.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Harry Potter and the Sorcerers Stone"],
+  },
+  {
+    name: "Stephen King",
+    imageURL: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Stephen_King%2C_Comicon.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Shining", "IT"],
+  },
+  {
+    name: "Charles Dickens",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/5/56/Charles_Dickens_by_Herbert_Watkins_29_April_1858_%28alternate%29.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["David Cooperfield", "A Tale of Two Cities"],
+  },
+  {
+    name: "William Shakespeare",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Shakespeare_Droeshout_1623.jpg/1920px-Shakespeare_Droeshout_1623.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"],
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import * as Redux from "redux";
+import * as ReactRedux from "react-redux";
+import { shuffle, sample } from "lodash";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AddAuthorForm from "./AddAuthorForm";
+import AuthorQuiz from "./AuthorQuiz";
+import authors from "./Authors";
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find(author => author.books.some(title => title === answer)),
+  };
+}
+
+function reducer(state = { authors, turnData: getTurnData(authors), highlight: "" }, action) {
+  switch (action.type) {
+    case "ANSWER_SELECTED":
+      const isCorrect = state.turnData.author.books.some(book => book === action.answer);
+      return Object.assign({}, state, {
+        highlight: isCorrect ? "correct" : "wrong",
+      });
+    case "CONTINUE":
+      return Object.assign({}, state, {
+        highlight: "",
+        turnData: getTurnData(state.authors),
+      });
+    case "ADD_AUTHOR":
+      return Object.assign({}, state, {
+        authors: state.authors.concat([action.author]),
+      });
+    default:
+      return state;
+  }
+}
+
+let store = Redux.createStore(reducer);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ReactRedux.Provider store={store}>
+      <React.Fragment>
+        <Route exact path="/" component={AuthorQuiz} />
+        <Route path="/add" component={AddAuthorForm} />
+      </React.Fragment>
+    </ReactRedux.Provider>
+  </BrowserRouter>,
+  document.getElementById("root"),
+);
